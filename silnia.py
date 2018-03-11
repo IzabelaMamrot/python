38622a51b2b24d30c1f5d7ac0b55ea0e1c18db72
IzabelaMamrot <iza.mamrot@gmail.com>
2018-03-11 17:04:08 +0100

    Struktury Danych i zlozonosci

--- silnia.py
+++ silnia.py
@@ -1,10 +1,13 @@
 #silnia 3!=1*2*3
+
 print("podaj liczbe ")
 a=int(input())
-for i in range(1,a):
-    wynik=i*a
-    if wynik==0:
-        break
-    else:
-        pass
-print("Wyniki", wynik)
+def silnia():
+	for i in range(1,a):
+	wynik=i*a
+	if wynik==0:
+		break
+	else:
+		pass
+	print("Wyniki", wynik)
+silnia()
